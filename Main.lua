fb8d3ac5d3ac452af58af074fdba969a2c145750
Inteli914900ks <intel.i9.14900ks@gmail.com>
2025-03-23 11:54:42 +0100

    Fixed Main Handler

---
+++ Main.lua
@@ -0,0 +1,11 @@
+local c = game.Players.LocalPlayer.Character
+
+if c:FindFirstChild("Humanoid") then
+    local humanoid = c:FindFirstChild("Humanoid")
+    
+    if humanoid.RigType == Enum.HumanoidRigType.R15 then
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/Inteli914900ks/Headless-Korblox/refs/heads/main/R15.lua"))()
+    elseif humanoid.RigType == Enum.HumanoidRigType.R6 then
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/Inteli914900ks/Headless-Korblox/refs/heads/main/R6.lua"))()
+    end
+end
